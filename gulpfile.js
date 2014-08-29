c86504e3c7f10ab3e4fe53fd1a14bb116f81866f
Marco Salazar de Leon <msalazardeleon@gmail.com>
2014-08-28 19:08:55 -0700

    Added Bootstrap and Font-Awesome

--- gulpfile.js
+++ gulpfile.js
@@ -67,6 +67,17 @@ gulp.task('copy', [
     'copy:index.html',
     'copy:jquery',
     'copy:main.css',
+    'copy:bootstrap-css',
+    'copy:bootstrap-glyph1',
+    'copy:bootstrap-glyph2',
+    'copy:bootstrap-glyph3',
+    'copy:bootstrap-glyph4',
+    'copy:bootstrap-js',
+    'copy:fa-css',
+    'copy:fa-font1',
+    'copy:fa-font2',
+    'copy:fa-font3',
+    'copy:fa-font4',
     'copy:misc',
     'copy:normalize'
 ]);
@@ -87,6 +98,78 @@ gulp.task('copy:jquery', function () {
     return gulp.src(['node_modules/jquery/dist/jquery.min.js'])
                .pipe(plugins.rename('jquery-' + pkg.devDependencies.jquery + '.min.js'))
                .pipe(gulp.dest(dirs.dist + '/js/vendor'));
+});
+
+gulp.task('copy:bootstrap-css', function () {
+    return gulp.src(['node_modules/bootstrap/dist/css/bootstrap.min.css'])
+                .pipe(plugins.rename('bootstrap-' + pkg.devDependencies.bootstrap + '.min.css'))
+                .pipe(gulp.dest(template('<%= dist %>/css', dirs)));
+});
+
+gulp.task('copy:bootstrap-glyph1', function () {
+    return gulp.src(['node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.eot'])
+                .pipe(plugins.rename('glyphicons-halflings-regular.eot'))
+                .pipe(gulp.dest(template('<%= dist %>/fonts', dirs)));
+});
+
+gulp.task('copy:bootstrap-glyph2', function () {
+    return gulp.src(['node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.svg'])
+                .pipe(plugins.rename('glyphicons-halflings-regular.svg'))
+                .pipe(gulp.dest(template('<%= dist %>/fonts', dirs)));
+});
+
+gulp.task('copy:bootstrap-glyph3', function () {
+    return gulp.src(['node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf'])
+                .pipe(plugins.rename('glyphicons-halflings-regular.ttf'))
+                .pipe(gulp.dest(template('<%= dist %>/fonts', dirs)));
+});
+
+gulp.task('copy:bootstrap-glyph4', function () {
+    return gulp.src(['node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.woff'])
+                .pipe(plugins.rename('glyphicons-halflings-regular.woff'))
+                .pipe(gulp.dest(template('<%= dist %>/fonts', dirs)));
+});
+
+gulp.task('copy:bootstrap-js', function () {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js'])
+                .pipe(plugins.rename('bootstrap-' + pkg.devDependencies.bootstrap + '.min.js'))
+                .pipe(gulp.dest(template('<%= dist %>/js/vendor', dirs)));
+});
+
+gulp.task('copy:fa-css', function () {
+    return gulp.src(['node_modules/font-awesome/css/font-awesome.min.css'])
+                .pipe(plugins.rename('font-awesome-' + pkg.devDependencies['font-awesome'] + '.min.css'))
+                .pipe(gulp.dest(template('<%= dist %>/css', dirs)));
+});
+
+gulp.task('copy:fa-font1', function () {
+    return gulp.src(['node_modules/font-awesome/fonts/FontAwesome.otf'])
+                .pipe(plugins.rename('fontawesome-webfont.otf'))
+                .pipe(gulp.dest(template('<%= dist %>/fonts', dirs)));
+});
+
+gulp.task('copy:fa-font2', function () {
+    return gulp.src(['node_modules/font-awesome/fonts/fontawesome-webfont.eot'])
+                .pipe(plugins.rename('fontawesome-webfont.eot'))
+                .pipe(gulp.dest(template('<%= dist %>/fonts', dirs)));
+});
+
+gulp.task('copy:fa-font3', function () {
+    return gulp.src(['node_modules/font-awesome/fonts/fontawesome-webfont.svg'])
+                .pipe(plugins.rename('fontawesome-webfont.svg'))
+                .pipe(gulp.dest(template('<%= dist %>/fonts', dirs)));
+});
+
+gulp.task('copy:fa-font4', function () {
+    return gulp.src(['node_modules/font-awesome/fonts/fontawesome-webfont.ttf'])
+                .pipe(plugins.rename('fontawesome-webfont.ttf'))
+                .pipe(gulp.dest(template('<%= dist %>/fonts', dirs)));
+});
+
+gulp.task('copy:fa-font5', function () {
+    return gulp.src(['node_modules/font-awesome/fonts/fontawesome-webfont.woff'])
+                .pipe(plugins.rename('fontawesome-webfont.woff'))
+                .pipe(gulp.dest(template('<%= dist %>/fonts', dirs)));
 });
 
 gulp.task('copy:main.css', function () {
